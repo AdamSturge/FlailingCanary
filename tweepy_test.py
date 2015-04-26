8a9823837ad2f43664e9eaf61ee2c99063ee7d70
Jonny Lomond <j.j.lomond@mun.ca>
2015-04-26 15:15:41 -0230

    Trying again

--- tweepy_test.py
+++ tweepy_test.py
@@ -3,21 +3,13 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 import json
-
-keylist = open("config.txt").read().splitlines()
-
+import CanaryConfig as config
 
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-consumer_key    =keylist[0]
-consumer_secret =keylist[1]
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_token        =keylist[2]
-access_token_secret =keylist[3]
-
-
 
 def printKeyVals(data, indent=0):
     if isinstance(data, list):
@@ -64,9 +56,9 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
     l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_token_secret)
 
     stream = Stream(auth, l)
-    stream.filter(track=['@ClintonNews','@HillaryClinton'])
+    stream.filter(track=config.taglist)
 
